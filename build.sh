679655f3f1c0aa08f3e55326f963a8ce8aacb055
Mostafijur28 <mostafijur.riju@gmail.com>
2025-04-24 13:26:04 +0530

    Superuser

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
-cd bouquet_project
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Collect static files
+python bouquet_project/manage.py collectstatic --no-input
+
+# Run migrations
+python bouquet_project/manage.py migrate
+
+# Create superuser
+if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ] && [ "$DJANGO_SUPERUSER_EMAIL" ]; then
+    python bouquet_project/manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
